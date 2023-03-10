9d9a45ade9a214cc60d6557b7981e9b839d4018e
m.emamgholi <m.emamgholi@gmail.com>
2023-03-10 17:59:44 +0100

    Call functions in Program.cs

--- FFIT.ConsoleApp/Program.cs
+++ FFIT.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using FFIT.Service.FormatValidator;
+using FFIT.Service.MarkdownParser;
+using FFIT.Service.String;
+
+var stringService = new StringService();
+
+Console.WriteLine($"Middle of \"test\" is: {stringService.GetMiddle("test")}");
+
+var parser = new MarkdownParser();
+
+Console.WriteLine($"Parsed value of \"###### hellow world!\" is: {parser.ParseHeader("###### hellow world!")}");
+
+
+var formatValidator = new FormatValidator();
+
+Console.WriteLine($"Is \"12345\" parsable as a number? {formatValidator.IsFormat("12345","number")}");
+
