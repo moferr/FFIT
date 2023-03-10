71cb3a6fade7693a99e62fe4d8afe0ef8f01aa70
m.emamgholi <m.emamgholi@gmail.com>
2023-03-10 10:32:11 +0100

    Add StringService

---
+++ FFIT.Service/String/IStringService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFIT.Service.String
+{
+    public interface IStringService
+    {
+        /// <summary>
+        /// Returns Middle character of the string (If the string length is even, returns two middle characters); 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string GetMiddle(string input);
+    }
+}
---
+++ FFIT.Service/String/StringService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFIT.Service.String
+{
+    public class StringService
+    {
+        /// <summary>
+        /// Returns Middle character of the string (If the string length is even, returns two middle characters); 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string GetMiddle(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return string.Empty;
+            }
+
+            return input.Length % 2 == 1 ? input.Substring(input.Length / 2, 1) : input.Substring((input.Length / 2) - 1, 2);
+        }
+    }
+}
